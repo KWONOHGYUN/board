32901b583587d356fe0732eba8d0bdefffcbcb0e
KWONOHGYUN <83410167+KWONOHGYUN@users.noreply.github.com>
2022-01-24 23:58:56 +0900

    underbar animation 추가

--- public/animation/animation.js
+++ public/animation/animation.js
@@ -1,1 +1,11 @@
-// alert('g2')
+let underbar = $('#underbar');
+let navMenus = $('.nav a');
+console.log(navMenus);
+
+navMenus.click((menu) => slideunderbar(menu))
+
+function slideunderbar(menu) {
+    underbar.css('left', `${menu.currentTarget.offsetLeft}`+`px`);
+    underbar.css('width', `${menu.currentTarget.offsetWidth}`+`px`);
+    underbar.css('top', `${menu.currentTarget.offsetTop+menu.currentTarget.offsetHeight}`+`px`);
+}
